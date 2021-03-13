2b485f130d22a012b77adaee9bb8510a007f1924
Cariad Eccleston <cariad@hey.com>
2021-03-13 16:12:15 +0000

    Fix test

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -8,4 +8,4 @@ def test(logger: Logger) -> None:
     for path in Path(__file__).parent.joinpath("documents").iterdir():
         if path.is_file() and path.name.endswith(".md"):
             with open(path.with_suffix(".txt"), "r") as expect_stream:
-                assert from_file(path) == expect_stream.read()
+                assert from_file(path) + "\n" == expect_stream.read()
